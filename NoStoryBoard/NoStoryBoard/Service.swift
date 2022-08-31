2e1d800a376993e3673b6ff3f882827fe28d665e
IceAmerican0 <starsjun@naver.com>
2022-08-31 22:03:55 +0900

    MVVM -ing

--- NoStoryBoard/NoStoryBoard/Service.swift
+++ NoStoryBoard/NoStoryBoard/Service.swift
@@ -15,6 +15,12 @@ class Service {
     func fetch(onCompleted: @escaping (Model) -> Void) {
         repository.fetch { [weak self] entity in
             
+            guard let selected = entity.rounding_date else { return }
+            
+            let model = Model(responseData: selected)
+            self?.responseModel = model
+            
+            onCompleted(model)
         }
     }
 }
--- NoStoryBoard/NoStoryBoard/ViewModel.swift
+++ NoStoryBoard/NoStoryBoard/ViewModel.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+class ViewModel {
+    var onUpdated: () -> Void = {}
+    
+    var dateTimeString: String = "Loading.."
+    {
+        didSet {
+            onUpdated()
+        }
+    }
+    
+    let service = Service()
+    
+    
+    func reload() {
+        service.fetch{ [weak self] model in
+            guard let self = self else { return }
+            
+        }
+    }
+    
+    
+}

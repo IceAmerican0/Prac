7ae582c8a79c2fff12ebe19448a79c969435abe0
IceAmerican0 <starsjun@naver.com>
2022-12-05 22:47:52 +0900

    SwiftUI Start

---
+++ SwiftUI/FirstSwiftUI/FirstSwiftUI/FirstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIApp.swift
+//  FirstSwiftUI
+//
+//  Created by 박성준 on 2022/12/05.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

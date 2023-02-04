0110b8d3f51ed99d92b4222fec804045f410e8c2
starsjun@naver.com <starsjun@naver.com>
2023-02-04 22:56:16 +0900

    first viewController

--- toy/toy/SceneDelegate.swift
+++ toy/toy/SceneDelegate.swift
@@ -14,7 +14,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        
+         // NavigationController 설정
+        let navigationVC = UINavigationController(rootViewController: testViewController())
+        window?.rootViewController = navigationVC
+        
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {

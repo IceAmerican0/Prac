e1b606f6160a31868ccc15158aab6d5089a04d3f
IceAmerican0 <starsjun@naver.com>
2023-03-08 20:41:48 +0900

    Following Tutorial

---
+++ SwiftUI/Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 박성준 on 2023/03/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Turtle Rock")
+            .font(.title)
+            .foregroundColor(.green)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- SwiftUI/Landmarks/Landmarks/LandmarksApp.swift
+++ SwiftUI/Landmarks/Landmarks/LandmarksApp.swift
@@ -1,14 +1,14 @@
 //
-//  FirstSwiftUIApp.swift
-//  FirstSwiftUI
+//  LandmarksApp.swift
+//  Landmarks
 //
-//  Created by 박성준 on 2022/12/05.
+//  Created by 박성준 on 2023/03/08.
 //
 
 import SwiftUI
 
 @main
-struct FirstSwiftUIApp: App {
+struct LandmarksApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()

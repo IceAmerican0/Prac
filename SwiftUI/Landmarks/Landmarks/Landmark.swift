ebbfb7bfb0199635a648591d7b1148b3454431bd
IceAmerican0 <starsjun@naver.com>
2023-03-15 21:56:42 +0900

    Following Tutorial

---
+++ SwiftUI/Landmarks/Landmarks/Landmark.swift
@@ -0,0 +1,29 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by 박성준 on 2023/03/15.
+//
+
+import Foundation
+import SwiftUI
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: CoordinateSpace
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
